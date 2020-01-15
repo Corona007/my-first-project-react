546a326f8bacd630e24eb520bbc3f70414068e70
Esteban <eca121096@gmail.com>
2020-01-15 14:53:57 -0800

    initial commit

---
+++ src/components/MyNewComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div>
+                We are in MyNewComponent!
+            </div>
+        );
+    }
+}
+
+export default MyNewComponent;
